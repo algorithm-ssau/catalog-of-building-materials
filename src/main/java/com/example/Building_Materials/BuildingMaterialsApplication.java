62d3dbbd1f832a950d842317d2faf742fdd69091
xHlpNw <aleksashin990@gmail.com>
2025-03-13 20:32:16 +0400

    Initializing commit

---
+++ src/main/java/com/example/Building_Materials/BuildingMaterialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Building_Materials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingMaterialsApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
